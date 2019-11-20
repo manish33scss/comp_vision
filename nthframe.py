cf154435d6ffaa80377c391abc7531b3c6eae08c
manish kumar <44940688+manish33scss@users.noreply.github.com>
2019-11-20 10:53:52 +0530

    Add files via upload

---
+++ nthframe.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 19 23:20:22 2019
+
+@author: Manish
+"""
+
+import cv2
+ 
+# Opens the Video file
+cap= cv2.VideoCapture(r'D:\duh\project\data\p10.avi')
+i=1
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    if i%15 == 0:
+        cv2.imwrite('project'+str(i)+'.jpg',frame)
+    i+=1
+ 
+cap.release()
+cv2.destroyAllWindows()
