78be94764c41bfb4a0a86302f72dd7a08ec3cc1e
manish kumar <44940688+manish33scss@users.noreply.github.com>
2021-10-27 12:52:45 +0530

    Update convertimage2videocv.py

--- convertimage2videocv.py
+++ convertimage2videocv.py
@@ -1,16 +1,17 @@
 import cv2
 import numpy as np
 import glob
+import os
  
 img_array = []
-for filename in glob.glob(r'C:\Users\Manish\Desktop\ppts\Computer vision\prac_3d\downloads\pr\*'):
-    img = cv2.imread(filename)
+for filename in os.scandir(r"D:\Work\Data\steps"):
+    img = cv2.imread(filename.path)
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
  
  
-out = cv2.VideoWriter('project.avi',cv2.VideoWriter_fourcc(*'DIVX'), 15, size)
+out = cv2.VideoWriter('try1.avi',cv2.VideoWriter_fourcc(*'DIVX'), 24, size)
  
 for i in range(len(img_array)):
     out.write(img_array[i])
